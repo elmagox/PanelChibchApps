4771ee3ec9af4172479a823e620fac320db33131
IVAN MANGONES <elmangones@hotmail.com>
2022-01-17 09:24:30 -0500

    Seeder for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name'=> 'IVAN MANGONES',
+            'email'=> 'elmangones@hotmail.com',
+            'password'=> bcrypt('89050469809'),
+            'role_id'=> 1,
+            'is_active'=> 1,
+        ]);
     }
 }
